78b2e29c1a4467b8530149be63486a064a924540
Dihan de Bod <u21451193@up.ac.za>
2023-04-14 18:02:16 +0200

    asdf

---
+++ API/Migrations/20230414154841_Dihan.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Dihan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Admins_Users_UserID",
+                table: "Admins");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Admins_UserID",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Admins");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "Admins",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admins_UserID",
+                table: "Admins",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Admins_Users_UserID",
+                table: "Admins",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "UserID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
